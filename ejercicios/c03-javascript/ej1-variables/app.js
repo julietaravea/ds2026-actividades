d9b500af946ca49bf422a017663cd52013669002
julietaravea <juliraveabander@gmail.com>
2026-04-16 02:42:42 -0300

    Ejercicio 1 - Variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,13 @@
+const nombre = "Julieta";
+let edad = 20;
+const materia = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y estoy cursando ${materia}`);
+
+let contador = 0;
+
+contador += 1;
+contador += 1;
+contador += 1;
+
+console.log("El valor final del contador es:", contador);
