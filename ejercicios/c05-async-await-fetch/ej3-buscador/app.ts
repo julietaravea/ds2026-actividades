e17d8b682deb6f4975c4e82c5e60b7eba4d748e8
julietaravea <juliraveabander@gmail.com>
2026-05-03 03:31:48 -0300

    Ejercicio 3 - Buscador de libros

---
+++ ejercicios/c05-async-await-fetch/ej3-buscador/app.ts
@@ -0,0 +1,88 @@
+interface LibroOL {
+    title: string;
+    author_name?: string[];
+    first_publish_year?: number;
+}
+
+const input = document.getElementById("inputBusqueda") as HTMLInputElement;
+const boton = document.getElementById("btnBuscar") as HTMLButtonElement;
+const resultados = document.getElementById("resultados") as HTMLDivElement;
+const msjError = document.getElementById("error") as HTMLParagraphElement;
+const msjCargando = document.getElementById("cargando") as HTMLParagraphElement;
+
+
+async function buscarLibros(q: string): Promise<LibroOL[]> {
+    try {
+        const response = await fetch(`https://openlibrary.org/search.json?q=${q}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP ${response.status}`);
+        }
+
+        const data: { docs: LibroOL[] } = await response.json();
+        return data.docs;
+
+    } catch (error) {
+        msjError.textContent = "Error al buscar libros";
+        msjError.style.display = "block";
+        return [];
+    }
+}
+
+
+async function mostrarLibros() {
+    const q = input.value.trim();
+
+    // Validación
+    if (q === "") {
+        msjError.textContent = "Por favor ingresá un libro";
+        msjError.style.display = "block";
+        resultados.innerHTML = "";
+        return;
+    }
+
+    msjError.textContent = "";
+    msjError.style.display = "none";
+    resultados.innerHTML = "";
+
+    msjCargando.textContent = "Cargando...";
+
+    const libros = await buscarLibros(q);
+
+    msjCargando.textContent = "";
+
+    if (libros.length === 0) {
+        msjError.textContent = "No se encontraron resultados";
+        msjError.style.display = "block";
+        return;
+    }
+
+    // Mostrar 10
+    libros.slice(0, 10).forEach(libro => {
+
+        const card = document.createElement("div");
+        card.className = "card";
+
+        const titulo = document.createElement("h3");
+        titulo.textContent = libro.title;
+
+        const autor = document.createElement("p");
+        autor.textContent = libro.author_name
+            ? `Autor: ${libro.author_name[0]}`
+            : "Autor: desconocido";
+
+        const anio = document.createElement("p");
+        anio.textContent = libro.first_publish_year
+            ? `Año: ${libro.first_publish_year}`
+            : "Año: desconocido";
+
+        card.appendChild(titulo);
+        card.appendChild(autor);
+        card.appendChild(anio);
+
+        resultados.appendChild(card);
+    });
+}
+
+
+boton.addEventListener("click", mostrarLibros);
